7d534080efc612819950fa2fced57a87d03363d1
Stephen2Page <stephen2page@gmail.com>
2016-10-26 09:03:50 -0400

    completed

---
+++ WeatherAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherAPIDemo.Models;
+
+namespace WeatherAPIDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+        public ActionResult GetWeather()
+        {
+            Weather weath = new Weather();
+            return Json(weath.getWeatherForcast(), JsonRequestBehavior.AllowGet);
+            
+        }
+    }
+}
---
+++ WeatherAPIDemo/Models/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherAPIDemo.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string appid = "f9ef6ff16c06ec734352c3dab6dc88d5";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=" + appid + "&units=imperial";
+            //synchronous client.
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+    }
+}
